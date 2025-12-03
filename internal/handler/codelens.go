bf3a54dae1addb0aef633e7a11408b9e76ac98e7
benshi <807629978@qq.com>
2025-12-03 07:57:58 +0000

    Implement error handling for missing documents and enhance CodeLens generation by extracting statements from the document text.

--- internal/handler/codelens.go
+++ internal/handler/codelens.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	"github.com/sourcegraph/jsonrpc2"
 	"github.com/sqls-server/sqls/internal/lsp"
@@ -18,14 +19,36 @@ func (s *Server) handleTextDocumentCodeLens(ctx context.Context, conn *jsonrpc2.
 		return nil, err
 	}
 
-	codeLens := []lsp.CodeLens{
-		{
-			Range: params.Range,
+	f, ok := s.files[params.TextDocument.URI]
+	if !ok {
+		return nil, fmt.Errorf("document not found: %s", params.TextDocument.URI)
+	}
+	text := f.Text
+	stmts, err := getStatements(text)
+	if err != nil {
+		return nil, err
+	}
+
+	codeLens := []lsp.CodeLens{}
+	for _, stmt := range stmts {
+		r := lsp.Range{
+			Start: lsp.Position{
+				Line:      stmt.Pos().Line,
+				Character: stmt.Pos().Col,
+			},
+			End: lsp.Position{
+				Line:      stmt.End().Line,
+				Character: stmt.End().Col,
+			},
+		}
+		codeLens = append(codeLens, lsp.CodeLens{
+			Range: r,
 			Command: &lsp.Command{
 				Title:   "Execute Query",
 				Command: CommandExecuteQuery,
 			},
-		},
+		})
 	}
+
 	return codeLens, nil
 }
